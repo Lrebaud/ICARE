0aac4160b3e91a22b219f7361aea5a3708cf68b8
Louis Rebaud <louis.rebaud@gmail.com>
2022-09-21 11:17:05 +0200

    update setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='icare',
-    version='0.0.3',
+    version='0.0.4',
     description='ICARE models',
     long_description=long_description,
     long_description_content_type='text/markdown',
